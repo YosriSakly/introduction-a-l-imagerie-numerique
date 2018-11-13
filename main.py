70ea64a35695f978136c92944b3962c1cecb9c97
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2018-11-13 11:16:24 +0100

    All the steps are there, result not good however: type/normalization problem very likely

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def saliency_maps(images_tuple, lapsize, rg, sigmag):
 
 
 # Weight map function (corresponds to equation (14) from the arcticle)
-def weight_map_operator(images_tuple):
+def weight_map_construction(images_tuple):
     # Intialize weight maps to zero
     dims = images_tuple[0].shape
     nims = len(images_tuple)
@@ -51,6 +51,7 @@ def weight_map_operator(images_tuple):
     return weight_maps
 
 
+# Produce refined weights maps using guided filtering (Function for equations 15 and 16 of the article)
 def refined_weight_maps(guides_tuple, images_tuple, r, eps):
     nims = len(images_tuple)
     outputs = []
@@ -60,7 +61,29 @@ def refined_weight_maps(guides_tuple, images_tuple, r, eps):
     return tuple(outputs)
 
 
+# Pixel by pixel normalization
+def weight_maps_normalization(weights_tuple):
+    nims = len(weights_tuple)
+    normalization_image = np.zeros(weights_tuple[0].shape)
+    weights_list = list(weights_tuple)
+    for i in range(0, nims):
+        weights_list[i] = weights_list[i].astype(np.float)
+        normalization_image += weights_list[i]
+    for i in range(0, nims):
+        weights_list[i] *= 1 / normalization_image
+    return tuple(weights_list)
+
+
+def images_weighted_average(weights_tuple, images_tuple):
+    return np.sum(np.array(weights_tuple) * np.array(images_tuple), axis=0)
+
+
+
+
+
+
 ##################### Loadings and other prerequisites #############################################################
+
 # Path to the data
 path = os.getcwd() + "/Data/"
 
@@ -73,17 +96,19 @@ cv2.imshow("jug2", image2)
 
 
 # ################### A : Two scales image decomposition ###########################################################
+
 images = (image1, image2)
 # Size of averaging kernel for basis/details decomposition
 avg_size = 31
 # Get basis and details images
-basis_tuple, details_tuple = basis_details_decomposition(images, avg_size)
+bases_tuple, details_tuple = basis_details_decomposition(images, avg_size)
 # See result of basis/details decomposition
-cv2.imshow("basis1", basis_tuple[0])
+cv2.imshow("basis1", bases_tuple[0])
 cv2.imshow("details1", details_tuple[0])
 
 
 # ################### B : Weight map construction with guided filtering ############################################
+
 # Laplacian filter size
 lapsize = 3
 # Parameters for Gaussian lowpass filter
@@ -93,20 +118,32 @@ sigmag = 5
 saliency_tuple = saliency_maps(images, lapsize, rg, sigmag)
 # cv2.imshow("saliency1", saliency_tuple[1])
 
-
 # Get the weight maps
-weight_maps = weight_map_operator(saliency_tuple)
+weight_maps = weight_map_construction(saliency_tuple)
 # Vizualize weight map, we multiply by 255 to distinguish the pixel equal to 1 from those equal to 0
 plt.figure()
 plt.imshow(255 * weight_maps[0])
 plt.figure()
 plt.imshow(255 * weight_maps[1])
 
-
 # Refined weight maps using guided filtering
 eps1 = 0.1
 r1 = 10
-refined_wm_basis = refined_weight_maps(weight_maps, basis_tuple, r1, eps1)
+refined_wm_basis = refined_weight_maps(weight_maps, bases_tuple, r1, eps1)
 eps2 = 0.1
 r2 = 10
 refined_wm_details = refined_weight_maps(weight_maps, details_tuple, r2, eps2)
+
+# Normalization of weight maps
+refined_normalized_bases = weight_maps_normalization(refined_wm_basis)
+refined_normalized_details = weight_maps_normalization(refined_wm_details)
+
+# Fused basis and fused details
+fused_bases = images_weighted_average(refined_normalized_bases, bases_tuple)
+fused_details = images_weighted_average(refined_normalized_details, details_tuple)
+
+
+# Fused image
+fused_image = fused_bases + fused_details
+fused_image_uint8 = cv2.convertScaleAbs(fused_image)
+cv2.imshow("Fused image", fused_image_uint8)
